150a397be067b6c8d42a5f87b49910948031355b
rf1112 <72556403+rf1112@users.noreply.github.com>
2021-03-23 19:49:31 +0000

    Get GameObject with a marker name
    Getting GameObject on the floor map using the marker name to make the code compact.

--- Assets/Scripts/MarkerScanner.cs
+++ Assets/Scripts/MarkerScanner.cs
@@ -12,9 +12,9 @@ public class MarkerScanner : MonoBehaviour
     public GameObject minimap;
     public GameObject minimapCamera;
     public GameObject ARCamera;
-    public GameObject bee;
-    public GameObject butterfly;
-    public GameObject daisy;
+    //public GameObject bee;
+    //public GameObject butterfly;
+    //public GameObject daisy;
     public GameObject map;
     public bool scanned = false;
     public GameObject anchor;
@@ -47,27 +47,12 @@ public class MarkerScanner : MonoBehaviour
             if (updatedImage.trackingState == TrackingState.Tracking && !scanned)
             {
                 scanned = true;
-                if (updatedImage.referenceImage.name == "bee")
-                {
-                    map.transform.eulerAngles = new Vector3(0, map.transform.eulerAngles.y - bee.transform.eulerAngles.y, 0);
-                    minimapCamera.transform.position = bee.transform.position;
-                    anchor.transform.position = ARCamera.transform.position;
-                    anchor.transform.rotation = ARCamera.transform.rotation;
-                }
-                if (updatedImage.referenceImage.name == "butterfly")
-                {
-                    map.transform.eulerAngles = new Vector3(0, map.transform.eulerAngles.y - butterfly.transform.eulerAngles.y, 0);
-                    minimapCamera.transform.position = butterfly.transform.position;
-                    anchor.transform.position = ARCamera.transform.position;
-                    anchor.transform.rotation = ARCamera.transform.rotation;
-                }
-                if (updatedImage.referenceImage.name == "daisy")
-                {
-                    map.transform.eulerAngles = new Vector3(0, map.transform.eulerAngles.y - daisy.transform.eulerAngles.y, 0);
-                    minimapCamera.transform.position = daisy.transform.position;
-                    anchor.transform.position = ARCamera.transform.position;
-                    anchor.transform.rotation = ARCamera.transform.rotation;
-                }
+                GameObject marker = GameObject.Find(updatedImage.referenceImage.name);
+                map.transform.eulerAngles = new Vector3(0, map.transform.eulerAngles.y - marker.transform.eulerAngles.y, 0);
+                minimapCamera.transform.position = marker.transform.position;
+                anchor.transform.position = ARCamera.transform.position;
+                anchor.transform.rotation = ARCamera.transform.rotation;
+
             }
         }
 
